5fa88e20ad2ccf91a329a66f192c5395804c43da
StrawKat111 <ezegyemail1000@gmail.com>
2024-11-22 10:32:59 +0100

    Add project files.

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KörHenger
+{
+    public class Kor
+    {
+        protected double sugar,
+            terület,
+            kerület;
+
+        public Kor() { }
+
+        public Kor(double r)
+        {
+            this.sugar = r;
+        }
+
+        public void SetSugar(double r)
+        {
+            this.sugar = r;
+        }
+
+        public void SetTerület()
+        {
+            this.terület = Math.Round(Math.Pow(this.sugar, 2) * Math.PI, 2);
+        }
+
+        public void SetKerület()
+        {
+            this.kerület = Math.Round(2 * this.sugar * Math.PI, 2);
+        }
+
+        public double GetTerület()
+        {
+            return this.terület;
+        }
+
+        public double GetKerület()
+        {
+            return this.kerület;
+        }
+
+        public double GetSugár()
+        {
+            return this.sugar;
+        }
+    }
+
+    class Hengertok : Kor
+    {
+        private double térfogat,
+            magasság;
+
+        public Hengertok(double s, double m)
+        {
+            this.sugar = s;
+            this.magasság = m;
+
+            SetKerület();
+            SetTerület();
+            this.térfogat = this.terület * this.magasság;
+        }
+
+        public double GetTérfogat()
+        {
+            return this.térfogat;
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using KörHenger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Kor k1 = new Kor();
+
+            k1.SetSugar(15);
+
+            k1.SetKerület();
+
+            k1.SetTerület();
+
+            kiír(k1);
+
+
+            Kor k2 = new Kor(58);
+
+            k2.SetKerület();
+
+            k2.SetTerület();
+
+            kiír(k2);
+
+            Console.ReadKey();
+
+        }
+
+        private static void kiír(Kor k)
+
+        {
+
+            Console.WriteLine($"A {k.GetSugár()} sugarú kör kerülete: {k.GetKerület()}, területe: {k.GetTerület()}.");
+
+        }
+
+
+    }
+}
